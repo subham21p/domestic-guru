5f957306e6946d2dcdd0cbcbe5d2c5fc1d7a7e83
Subham <subham.k.patra@gmail.com>
2021-10-30 14:55:20 +0530

    Make Changes

--- app/src/main/java/com/app/domesticguru/ui/fragment/LogInFragment.kt
+++ app/src/main/java/com/app/domesticguru/ui/fragment/LogInFragment.kt
@@ -24,10 +24,6 @@ import com.app.domesticguru.utils.NetworkStatus
 import com.app.domesticguru.utils.SharedPreferenceManager
 import java.util.Observer
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-/*private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"*/
 
 
 class LogInFragment : Fragment() {
